e8d3c575a8a164a6ac9f9d8a51d715d29c134978
stefan.schwetschke <stefan@schwetschke.de>
2023-03-02 14:01:00 +0100

    fix(setup): Cleanup and remove broken steps

--- setup.sh
+++ setup.sh
@@ -1,5 +1,29 @@
 #! /bin/sh
 
+# set -Eeuo pipefail
+# IFS=$'\n\t'
+
+run_as_root() {
+  if [ "$(id -u)" = "0" ]; then
+    # we are already root, so just run the command
+    "$@"
+  elif command -v sudo >/dev/null 2>&1
+  then
+    # sudo is installed, so use it to run the command
+    sudo "$@"
+  elif command -v doas >/dev/null 2>&1
+  then
+    # doas is installed, so use it to run the command
+    doas "$@"
+  else
+    # neither sudo nor doas is installed, so print an error message
+    echo "Error: neither sudo nor doas is installed." >&2
+    return 1
+  fi
+}
+
+
+
 mkdir -p "${HOME}/.config/nixpkgs/"
 cat > "${HOME}/.config/nixpkgs/home.nix" <<EOF
 
@@ -22,33 +46,23 @@ cat > "${HOME}/.config/nixpkgs/home.nix" <<EOF
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "20.09";
+  home.stateVersion = "22.11";
 
   home.packages = [
     pkgs.htop
-    pkgs.fortune
-    (pkgs.nerdfonts.override { fonts = [ "Iosevka" "VictorMono" ]; })
+    # (pkgs.nerdfonts.override { fonts = [ "Iosevka" "VictorMono" ]; })
     pkgs.ripgrep
     pkgs.fd
     pkgs.watchman
     pkgs.curl
     pkgs.curlie
-    pkgs.sops
     pkgs.mcfly
-    pkgs.visidata
-    pkgs.glances
-    pkgs.dive
-    pkgs.mkcert
-    pkgs.step-cli
-    pkgs.hexyl
     pkgs.pv
     pkgs.shellcheck
-    pkgs.pueue
-    pkgs.vips
-    pkgs.gitAndTools.git-trim
+    # pkgs.gitAndTools.git-trim
   ];
 
-  fonts.fontconfig.enable = true;
+  # fonts.fontconfig.enable = true;
 
   programs.bat.enable = true;
   programs.jq.enable = true;
@@ -61,63 +75,27 @@ cat > "${HOME}/.config/nixpkgs/home.nix" <<EOF
 
   programs.tmux.enable = true;
 
-  programs.emacs = {
-    enable = true;
-    package = pkgs.emacs-nox;
-  };
-
   programs.vim.enable = true; 
-
-  #home.file.".emacs.d" = {
-  #  # don't make the directory read only so that impure melpa can still happen
-  #  # for now
-  #  recursive = true;
-  #  source = pkgs.fetchFromGitHub {
-  #    owner = "syl20bnr";
-  #    repo = "spacemacs";
-  #    rev = "7e38f2e64e1c5cc74966cdbccf6b4adc2f89fc44";
-  #    sha256 = "0bjbmk4c7f1pr5vrpbapv594mcz379krvwvfzs4g91yvmsva6g9z";
-  #  };
-  #};
-
-  # spacemacs
-  home.file = { 
-    ".emacs.d" = {
-       source = pkgs.fetchFromGitHub {
-         owner = "syl20bnr";
-         repo = "spacemacs";
-         rev = "1f93c05";
-         sha256 = "1x0s5xlwhajgnlnb9mk0mnabhvhsf97xk05x79rdcxwmf041h3fd";
-       };
-       recursive = true;
-    };
-  };
-  
-  home.activation = {
-    spacemacs = ''
-      $DRY_RUN_CMD touch ~/.spacemacs
-    '';
-  };
 }
 EOF
 
 
 if type apt-get
 then
-  sudo apt-get update
-  sudo env DEBIAN_FRONTEND=noninteractive apt-get -y install curl xz-util
+  run_as_root apt-get update
+  run_as_root env DEBIAN_FRONTEND=noninteractive apt-get -y install curl
 fi
 
-curl -L https://nixos.org/nix/install | sh
-. "${HOME}/.nix-profile/etc/profile.d/nix.sh"
-nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install linux --no-confirm --init none
+. /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+# nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
-if type home-manager
-then
-  home-manager switch
-else
-  nix-shell '<home-manager>' -A install
-  . "${HOME}/.nix-profile/etc/profile.d/hm-session-vars.sh"
-fi
-. "${HOME}/.nix-profile/etc/profile.d/hm-session-vars.sh"
+# if command -v home-manager > /dev/null 2>&1
+# then
+#   home-manager switch
+# else
+#   nix-shell '<home-manager>' -A install
+#   . "${HOME}/.nix-profile/etc/profile.d/hm-session-vars.sh"
+# fi
+# . "${HOME}/.nix-profile/etc/profile.d/hm-session-vars.sh"
 
