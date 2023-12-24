feb834f23dfde4d2ce882e6d6033324ad90ac254
stefan.schwetschke <stefan@schwetschke.de>
2023-12-24 19:51:29 +0100

    feat(packages): Add many packages

--- home.nix
+++ home.nix
@@ -12,8 +12,66 @@
   programs.direnv.nix-direnv.enable = true;
   
   programs.zsh.enable = true;
-  programs.fish.enable = true;
+  programs.fish = {
+    enable = true;
+    plugins = [
+      { name = "z"; src = pkgs.fishPlugins.z.src; }
+      { name = "fzf"; src = pkgs.fishPlugins.fzf-fish.src; }
+      { name = "forgit"; src = pkgs.fishPlugins.forgit.src; }
+      { name = "github-copilot-cli"; src = pkgs.fishPlugins.github-copilot-cli-fish.src; }
+      { name = "bass"; src = pkgs.fishPlugins.bass.src; }
+    ];
+    shellAbbrs = {
+      # Abbreviations for "forgit": https://github.com/wfxr/forgit
+      # You can also use completion on "forgit::"
+      "+git-add-interactive" = "ga";
+      "+git-checkout-branch" = "gcb";
+      "+git-checkout-commit" = "gco";
+      "+git-checkout-file" = "gcf";
+      "+git-checkout-tag" = "gct";
+      "+git-commit-fixup" = "gfu";
+      "+git-delete-branch-interactive" = "gbd";
+      "+git-diff-interactive" = "gd";
+      "+git-ignore-generator" = "gi";
+      "+git-log-viewer" = "glo";
+      "+git-reset-head" = "grh";
+      "+git-revert-commit" = "grc";
+      "+git-stash-push" = "gsp";
+      "+git-stash-viewer" = "gss";
+
+      # lsd dir colors
+      "+l" = "lsd";
+      "+la" = "lsd -a";
+      "+ll" = "lsd -l";
+      "+lla" = "lsd -la";
+      "+llt" = "lsd --long --tree --ignore-glob .git --ignore-glob node_modules";
+      "+lt" = "lsd --tree --ignore-glob .git --ignore-glob node_modules";
+
+    };
+  };
   programs.starship.enable = true;
+
+  programs.git = {
+    enable = true;
+    userName = "stefan.schwetschke";
+    userEmail = "stefan@schwetschke.de";
+    lfs = {
+      enable = true;
+    };
+    difftastic = {
+      # Note: For big files, use "delta" instead. 
+      # It's faster, also has syntax highlightning, but doesn't interpret structure.
+      enable = true;
+    };
+  };
+  programs.gh.enable = true;
+  programs.lazygit.enable = true;
+  
+  programs.bat.enable = true;
+  programs.fzf.enable = true;
+  programs.lsd.enable = true;
+  programs.mcfly.enable = true;
+  programs.ripgrep.enable = true;
 
   # Htop
   # https://rycee.gitlab.io/home-manager/options.html#opt-programs.htop.enable
@@ -25,7 +83,6 @@
     coreutils
     curl
     wget
-    comma
 
     # Dev stuff
     # (agda.withPackages (p: [ p.standard-library ]))
@@ -34,12 +91,17 @@
     nodePackages.ts-node
     nodejs
 
+    # Command line helper
+
     # Useful nix related tools
     cachix # adding/managing alternative binary caches hosted by Cachix
-    # comma # run software from without installing it
-    nodePackages.node2nix
+    comma # run software from without installing it
+    nodePackages.node2nix # Convert node packages to Nix
+
+    chatblade
 
   ] ++ lib.optionals stdenv.isDarwin [
+    mas # CLI for the macOS app store
     m-cli # useful macOS CLI commands
   ];
 
@@ -50,9 +112,9 @@
     templates = {
       scm-init = "git";
       params = {
-        author-name = "Your Name"; # config.programs.git.userName;
-        author-email = "youremail@example.com"; # config.programs.git.userEmail;
-        github-username = "yourusername";
+        author-name = "Stefan Schwetschke"; # config.programs.git.userName;
+        author-email = "stefan@schwetschke.de"; # config.programs.git.userEmail;
+        github-username = "geggo99";
       };
     };
     nix.enable = true;
