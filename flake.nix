24d5aebf989b931755b98be815bbf3476181c62d
stefan.schwetschke <stefan@schwetschke.de>
2024-04-22 09:32:28 +0200

    feat(doc): Document installation step
    feat(doc): Doucument more installation steps

--- flake.nix
+++ flake.nix
@@ -1,4 +1,5 @@
 # darwin-rebuild switch --flake ~/.config/nix-darwin
+# git add . && nix --extra-experimental-features "nix-command flakes"  run nix-darwin -- switch --flake ~/.config/nix-darwin
 {
   description = "Stefan's darwin system";
 
