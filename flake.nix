ea73e1e3d435b0384a576101ccd2f980c34c574d
stefan.schwetschke <stefan@schwetschke.de>
2024-10-16 19:09:15 +0200

    chore(nix): Switch to channel nixpkgs-24.05-darwin

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
 
   inputs = {
     # Package sets
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-23.11-darwin"; # https://status.nixos.org/
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-24.05-darwin"; # https://status.nixos.org/
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     # Environment/system management
