efc9e61aefab2d556da4a6300938d3cfc64b0830
stefan.schwetschke <stefan@schwetschke.de>
2024-10-18 21:57:10 +0200

    doc(nix): Document single user mode

--- hosts/DKL6GDJ7X1/configuration.nix
+++ hosts/DKL6GDJ7X1/configuration.nix
@@ -4,6 +4,7 @@
   nix.useDaemon = lib.mkForce false;
   services.nix-daemon.enable = lib.mkForce false;
   nix.gc.user = "stefan.schwetschke";
+  # sudo chmod +a "${USER} allow read,write,execute,delete,readattr,writeattr,readextattr,writeextattr,readsecurity,writesecurity,chown,add_file,add_subdirectory,delete_child,file_inherit,directory_inherit" /nix/store
 
   # For Sequoia migration run: `curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- repair sequoia --move-existing-users`
   ids.uids.nixbld = 300;
