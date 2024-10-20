7042eca85ac0abffba1b6f8f56a52e03f0b827fa
Stefan Schwetschke <stefan@schwetschke.de>
2024-10-20 20:49:53 +0200

    fix(nix): Fix for old group IDs of the build users

--- hosts/FCX19GT9XR/configuration.nix
+++ hosts/FCX19GT9XR/configuration.nix
@@ -3,6 +3,9 @@
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
 
+  # Left-over from macOS Sequoia migration. Remove after re-install
+  ids.gids.nixbld = 30000;
+
   # For home-manager
   users.users.stefan.home = /Users/stefan;
 }
