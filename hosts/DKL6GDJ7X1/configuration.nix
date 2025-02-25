ee1ea9f21a1f988d23d8c64039a6a7191f99bf60
stefan.schwetschke <stefan@schwetschke.de>
2025-02-25 15:07:28 +0100

    feat(nix): Migrate to regular Nix daemon

--- hosts/DKL6GDJ7X1/configuration.nix
+++ hosts/DKL6GDJ7X1/configuration.nix
@@ -1,15 +1,15 @@
 { pkgs, lib, ... }:
 {
   # Disable the nix-daemon, since it clashes with Falcon CrowdStrike
-  nix.useDaemon = lib.mkForce false;
-  services.nix-daemon.enable = lib.mkForce false;
-  nix.gc.user = "stefan.schwetschke";
+  # nix.useDaemon = lib.mkForce false;
+  # services.nix-daemon.enable = lib.mkForce false;
+  # nix.gc.user = "stefan.schwetschke";
   # sudo chmod +a "${USER} allow read,write,execute,delete,readattr,writeattr,readextattr,writeextattr,readsecurity,writesecurity,chown,add_file,add_subdirectory,delete_child,file_inherit,directory_inherit" /nix/store
 
   # For Sequoia migration run: `curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- repair sequoia --move-existing-users`
-  ids.uids.nixbld = 300;
+  # ids.uids.nixbld = 300;
   # With Seqoia, the Nix build user group ID must be changed from 30000 to 350
-  ids.gids.nixbld = 30000;
+  # ids.gids.nixbld = 30000;
 
   # For home-manager
   users.users."stefan.schwetschke".home = /Users/stefan.schwetschke;
