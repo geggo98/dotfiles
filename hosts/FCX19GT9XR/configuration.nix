3ba6971711c0299ec131871601ea88131ee84cad
Stefan Schwetschke <stefan@schwetschke.de>
2025-03-09 12:40:42 +0100

    feat(packages): Update to Nix 11.24

--- hosts/FCX19GT9XR/configuration.nix
+++ hosts/FCX19GT9XR/configuration.nix
@@ -1,10 +1,10 @@
 { pkgs, lib, ... }:
 {
   # Auto upgrade nix package and the daemon service.
-  services.nix-daemon.enable = true;
+  # services.nix-daemon.enable = true;
 
   # Left-over from macOS Sequoia migration. Remove after re-install
-  ids.gids.nixbld = 30000;
+  # ids.gids.nixbld = 30000;
 
   # For home-manager
   users.users.stefan.home = /Users/stefan;
