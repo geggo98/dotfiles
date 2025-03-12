990b717e471d7888e92432931bcd17d3c89f8c51
stefan.schwetschke <stefan@schwetschke.de>
2025-03-12 12:23:12 +0100

    chore(home): Cleanup boundary scripts

--- hosts/DKL6GDJ7X1/home.nix
+++ hosts/DKL6GDJ7X1/home.nix
@@ -2,7 +2,7 @@
 {
   # This module appends to the existing home.packages list
   home.packages = with pkgs; [
-    (pkgs.writeShellScriptBin "boundary_auth" ./scripts/boundary_auth.sh)
-    (pkgs.writeShellScriptBin "boundary_connect" ./scripts/boundary_connect_defaults.sh)
+    (pkgs.writeShellScriptBin "+boundary_legacy_auth" ./scripts/boundary_legacy_auth.sec.sh)
+    (pkgs.writeShellScriptBin "+boundary_legacy_connect_defaults" ./scripts/boundary_legacy_connect_defaults.sec.sh)
   ];
 }
