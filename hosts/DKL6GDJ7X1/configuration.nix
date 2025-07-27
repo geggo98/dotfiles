3d4794f8c1b33d44bbd51d22ff3a9b965241d1f1
stefan.schwetschke <stefan@schwetschke.de>
2025-07-27 22:35:30 +0200

    feat(nix): Update nixpkgs and home-manager to 25.05

--- hosts/DKL6GDJ7X1/configuration.nix
+++ hosts/DKL6GDJ7X1/configuration.nix
@@ -4,7 +4,7 @@
   # nix.useDaemon = lib.mkForce false;
   # services.nix-daemon.enable = lib.mkForce false;
   # nix.gc.user = "stefan.schwetschke";
-  # sudo chmod +a "${USER} allow read,write,execute,delete,readattr,writeattr,readextattr,writeextattr,readsecurity,writesecurity,chown,add_file,add_subdirectory,delete_child,file_inherit,directory_inherit" /nix/store
+  # sudo chmod +a "${USER} allow read,write,execute,delete,readattr,writeattr,readextattr,writeextattr,chown,add_file,add_subdirectory,delete_child,file_inherit,directory_inherit" /nix/store
 
   # For Sequoia migration run: `curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- repair sequoia --move-existing-users`
   # ids.uids.nixbld = 300;
@@ -13,4 +13,5 @@
 
   # For home-manager
   users.users."stefan.schwetschke".home = /Users/stefan.schwetschke;
+  system.primaryUser = "stefan.schwetschke";
 }
