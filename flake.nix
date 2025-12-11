b58a04ac34f9dbee88d02bf0da4f18b7355bb8a5
stefan.schwetschke <stefan.schwetschke@check24.de>
2025-12-11 14:30:47 +0100

    feat(home): Add several agents

--- flake.nix
+++ flake.nix
@@ -16,95 +16,107 @@
     # Environment/system management
     darwin.url = "github:lnl7/nix-darwin/nix-darwin-25.11";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
-    
+
     home-manager.url = "github:nix-community/home-manager/release-25.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    
+
     # https://github.com/Mic92/sops-nix
     sops-nix.url = "github:Mic92/sops-nix";
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
-    
+
     # https://github.com/nix-community/nix-index-database
     nix-index-database.url = "github:Mic92/nix-index-database";
     nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
+
+    # https://github.com/numtide/llm-agents.nix
+    nixpkgs-llm-agents.url = "github:numtide/llm-agents.nix";
 
     # External dependencies
     astronvim = { url = "github:AstroNvim/AstroNvim/v3.40.1"; flake = false; };
   };
 
-  outputs = { self, darwin, nixpkgs, nixpkgs-unstable, home-manager, sops-nix, nix-index-database, ... }@inputs:
-  let 
-    inherit (darwin.lib) darwinSystem;
-    inherit (inputs.nixpkgs.lib) attrValues makeOverridable optionalAttrs singleton;
+  nixConfig = {
+    # Caches ----------------------------------------------------------------- {{{
+    # https://github.com/numtide/llm-agents.nix/blob/main/flake.nix
+    extra-substituters = [ "https://numtide.cachix.org" ];
+    extra-trusted-public-keys = [ "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE=" ];
+  };
 
-    # Configuration for `nixpkgs`
-    nixpkgsUnfreeConfig = {
-      config = { allowUnfree = true; };
-    }; 
-  in
-  {
-    # My `nix-darwin` configs
-      
-    darwinConfigurations = {
-      FCX19GT9XR = darwinSystem {
-        modules = [
-          { nixpkgs.hostPlatform = "aarch64-darwin"; }
-          # Main `nix-darwin` config
-          ./configuration.nix
-          ./darwin.nix
-          # Host specific packages
-          ./hosts/FCX19GT9XR/configuration.nix
-          ./hosts/FCX19GT9XR/homebrew.nix
-          # `home-manager` module
-          home-manager.darwinModules.home-manager {
-            # WARNING:
-            # Don't import the sops home-manager module here,
-            # it's a NixOS specific plugin and tries to use SystemD.
-            # You will get an error message like this:
-            # `error: The option `systemd' does not exist. Definition values: ...`
-            # So don't do this: `sops-nix.homeManagerModules.sops`
-            # Instead, import it in the `home.nix` file.
-            nixpkgs = nixpkgsUnfreeConfig;
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.stefan = import ./home.nix;
-            home-manager.extraSpecialArgs = inputs;
-          }
-        ];
+  outputs = { self, darwin, nixpkgs, nixpkgs-unstable, home-manager, sops-nix, nix-index-database, nixpkgs-llm-agents, ... }@inputs:
+    let
+      inherit (darwin.lib) darwinSystem;
+      inherit (inputs.nixpkgs.lib) attrValues makeOverridable optionalAttrs singleton;
+
+      # Configuration for `nixpkgs`
+      nixpkgsUnfreeConfig = {
+        config = { allowUnfree = true; };
       };
-      DKL6GDJ7X1 = darwinSystem {
-        modules = [
-          { nixpkgs.hostPlatform = "aarch64-darwin"; }
-          # Main `nix-darwin` config
-          ./configuration.nix
-          ./darwin.nix
-          # Host specific packages
-          ./hosts/DKL6GDJ7X1/configuration.nix
-          ./hosts/DKL6GDJ7X1/homebrew.nix
-          # `home-manager` module
-          home-manager.darwinModules.home-manager {
-            # WARNING:
-            # Don't import the sops home-manager module here,
-            # it's a NixOS specific plugin and tries to use SystemD.
-            # You will get an error message like this:
-            # `error: The option `systemd' does not exist. Definition values: ...`
-            # So don't do this: `sops-nix.homeManagerModules.sops`
-            # Instead, import it in the `home.nix` file.
-            nixpkgs = nixpkgsUnfreeConfig;
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users."stefan.schwetschke" = nixpkgs.lib.mkMerge [ (import ./home.nix) (import ./hosts/DKL6GDJ7X1/home.nix) ];
-            home-manager.extraSpecialArgs = inputs;            
-          }
-        ];
+    in
+    {
+      # My `nix-darwin` configs
+
+      darwinConfigurations = {
+        FCX19GT9XR = darwinSystem {
+          modules = [
+            { nixpkgs.hostPlatform = "aarch64-darwin"; }
+            # Main `nix-darwin` config
+            ./configuration.nix
+            ./darwin.nix
+            # Host specific packages
+            ./hosts/FCX19GT9XR/configuration.nix
+            ./hosts/FCX19GT9XR/homebrew.nix
+            # `home-manager` module
+            home-manager.darwinModules.home-manager
+            {
+              # WARNING:
+              # Don't import the sops home-manager module here,
+              # it's a NixOS specific plugin and tries to use SystemD.
+              # You will get an error message like this:
+              # `error: The option `systemd' does not exist. Definition values: ...`
+              # So don't do this: `sops-nix.homeManagerModules.sops`
+              # Instead, import it in the `home.nix` file.
+              nixpkgs = nixpkgsUnfreeConfig;
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.stefan = import ./home.nix;
+              home-manager.extraSpecialArgs = inputs;
+            }
+          ];
+        };
+        DKL6GDJ7X1 = darwinSystem {
+          modules = [
+            { nixpkgs.hostPlatform = "aarch64-darwin"; }
+            # Main `nix-darwin` config
+            ./configuration.nix
+            ./darwin.nix
+            # Host specific packages
+            ./hosts/DKL6GDJ7X1/configuration.nix
+            ./hosts/DKL6GDJ7X1/homebrew.nix
+            # `home-manager` module
+            home-manager.darwinModules.home-manager
+            {
+              # WARNING:
+              # Don't import the sops home-manager module here,
+              # it's a NixOS specific plugin and tries to use SystemD.
+              # You will get an error message like this:
+              # `error: The option `systemd' does not exist. Definition values: ...`
+              # So don't do this: `sops-nix.homeManagerModules.sops`
+              # Instead, import it in the `home.nix` file.
+              nixpkgs = nixpkgsUnfreeConfig;
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users."stefan.schwetschke" = nixpkgs.lib.mkMerge [ (import ./home.nix) (import ./hosts/DKL6GDJ7X1/home.nix) ];
+              home-manager.extraSpecialArgs = inputs;
+            }
+          ];
+        };
+
       };
 
-    };
+      # Overlays --------------------------------------------------------------- {{{
 
-    # Overlays --------------------------------------------------------------- {{{
-
-    overlays = {
-      # Overlay useful on Macs with Apple Silicon
+      overlays = {
+        # Overlay useful on Macs with Apple Silicon
         apple-silicon = final: prev: optionalAttrs (prev.stdenv.hostPlatform.system == "aarch64-darwin") {
           # Add access to x86 packages system is running Apple Silicon
           pkgs-x86 = import inputs.nixpkgs {
@@ -112,6 +124,6 @@
             inherit (nixpkgsUnfreeConfig) config;
           };
         };
+      };
     };
- };
 }
