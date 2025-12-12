42d0e4fd4cd78424f46be222fd35bab9bfc8ee94
stefan.schwetschke <stefan.schwetschke@check24.de>
2025-12-12 21:05:05 +0100

    feat(home): Rework outdated boundary scripts

--- hosts/DKL6GDJ7X1/home.nix
+++ hosts/DKL6GDJ7X1/home.nix
@@ -2,7 +2,5 @@
 {
   # This module appends to the existing home.packages list
   home.packages = with pkgs; [
-    (pkgs.writeShellScriptBin "+boundary_legacy_auth" ./scripts/boundary_legacy_auth.sec.sh)
-    (pkgs.writeShellScriptBin "+boundary_legacy_connect_defaults" ./scripts/boundary_legacy_connect_defaults.sec.sh)
   ];
 }
