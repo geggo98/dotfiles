c89483e4c238277b48d85b6173ff572064806159
Stefan Schwetschke <stefan@schwetschke.de>
2025-12-28 23:42:27 +0100

    feat(nix): Prepare for Linux builder

--- determinate.nix
+++ determinate.nix
@@ -8,5 +8,9 @@
   determinate-nix.customSettings = {
     # settings get written into /etc/nix/nix.custom.conf
     "download-buffer-size" = "1073741824"; # 1 GiB
+    # https://determinate.systems/blog/changelog-determinate-nix-384/
+    # https://dtr.mn/features 
+    # "extra-experimental-features" = "external-builders";
+    # "external-builders" = "[{\"systems\":[\"aarch64-linux\",\"x86_64-linux\"],\"program\":\"/usr/local/bin/determinate-nixd\",\"args\":[\"builder\"]}]";
   };
 }
