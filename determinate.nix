c3ec24087fafe9b9cb11ea00e0747ffd966479a8
Stefan Schwetschke <stefan@schwetschke.de>
2026-01-31 17:50:50 +0100

    feat(nix): Enable parallel evaluation

--- determinate.nix
+++ determinate.nix
@@ -9,6 +9,7 @@
     # settings get written into /etc/nix/nix.custom.conf
     "download-buffer-size" = "1073741824"; # 1 GiB
     "trusted-users" = "root stefan stefan.schwetschke";
+    "eval-cores" = "0"; # https://docs.determinate.systems/determinate-nix/#parallel-evaluation
     # https://determinate.systems/blog/changelog-determinate-nix-384/
     # https://dtr.mn/features 
     # "extra-experimental-features" = "external-builders";
