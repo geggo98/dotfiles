e57baef1085dbffbf9709c68eec53eaa6e52813f
stefan.schwetschke <stefan@schwetschke.de>
2026-02-17 10:00:26 +0100

    fix(nix): Update Darwin integration for Determinate Nix
    - fix for failing `determinate-nixd version` command

--- determinate.nix
+++ determinate.nix
@@ -1,18 +1,20 @@
-{ inputs, ... }:
-{
+{inputs, ...}: {
   imports = [
     inputs.determinate.darwinModules.default
   ];
 
   nix.enable = false; # Let Determinate manage Nix
-  determinate-nix.customSettings = {
-    # settings get written into /etc/nix/nix.custom.conf
-    "download-buffer-size" = "1073741824"; # 1 GiB
-    "trusted-users" = "root stefan stefan.schwetschke";
-    "eval-cores" = "0"; # https://docs.determinate.systems/determinate-nix/#parallel-evaluation
-    # https://determinate.systems/blog/changelog-determinate-nix-384/
-    # https://dtr.mn/features 
-    # "extra-experimental-features" = "external-builders";
-    # "external-builders" = "[{\"systems\":[\"aarch64-linux\",\"x86_64-linux\"],\"program\":\"/usr/local/bin/determinate-nixd\",\"args\":[\"builder\"]}]";
+  determinateNix = {
+    enable = true;
+    customSettings = {
+      # settings get written into /etc/nix/nix.custom.conf
+      "download-buffer-size" = "1073741824"; # 1 GiB
+      "trusted-users" = ["root" "stefan" "stefan.schwetschke"];
+      "eval-cores" = "0"; # https://docs.determinate.systems/determinate-nix/#parallel-evaluation
+      # https://determinate.systems/blog/changelog-determinate-nix-384/
+      # https://dtr.mn/features
+      # "extra-experimental-features" = "external-builders";
+      # "external-builders" = "[{\"systems\":[\"aarch64-linux\",\"x86_64-linux\"],\"program\":\"/usr/local/bin/determinate-nixd\",\"args\":[\"builder\"]}]";
+    };
   };
 }
