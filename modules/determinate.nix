1cd6be3f067d4c21f92982c62ba869ecd02af50b
Stefan Schwetschke <stefan@schwetschke.de>
2026-03-15 18:15:03 +0100

    feat(darwin): Update Nix darwin for determinate and enable Linux builder

--- modules/determinate.nix
+++ modules/determinate.nix
@@ -6,6 +6,13 @@
     nix.enable = false;
     determinateNix = {
       enable = true;
+      determinateNixd = {
+        builder = {
+          state = "enabled";
+          memoryBytes = 8589934592;
+          cpuCount = 1;
+        };
+      };
       customSettings = {
         "download-buffer-size" = "1073741824";
         "trusted-users" = [ "root" "stefan" "stefan.schwetschke" ];
